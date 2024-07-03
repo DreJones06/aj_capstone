3bc3b706d45447f8e1844510d1b0c5c1937cb301
Ubuntu <ubuntu@ip-172-31-19-38.ec2.internal>
2024-07-03 15:21:04 +0000

    latest

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,14 @@
 if [[ $GIT_BRANCH == "origin/Dev" ]]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u drejones06 
+#    docker login -u drejones06 
     docker tag devops-build drejones06/dev
     docker push drejones06/dev
 
 elif [[ $GIT_BRANCH == "origin/master" ]]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u drejones06 
+ #   docker login -u drejones06 
     docker tag devops-build drejones06/prod 
     docker push drejones06/prod
 else
