ba2cf1c1f743ac360e988cc1ccc713cee99c6436
DreJones06 <157946805+DreJones06@users.noreply.github.com>
2024-07-03 21:00:06 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+#Script developed to build docker in Dev and Prod based on the Git branch
 if [[ $GIT_BRANCH == "origin/Dev" ]]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
+        docker login -u drejones06 -p dckr_pat_wDWAxLoonygIJlzcpF75wsWL_d4
     docker tag devops-build drejones06/dev
     docker push drejones06/dev
 
 elif [[ $GIT_BRANCH == "origin/master" ]]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
+        docker login -u drejones06 -p dckr_pat_wDWAxLoonygIJlzcpF75wsWL_d4
     docker tag devops-build drejones06/prod 
     docker push drejones06/prod
 else
