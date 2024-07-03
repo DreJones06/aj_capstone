23a6ca3adc49db0c3024091fbd7d43b1340623f4
Ubuntu <ubuntu@ip-172-31-19-38.ec2.internal>
2024-07-03 15:15:45 +0000

    latest commit

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,16 @@
 if [[ $GIT_BRANCH == "origin/Dev" ]]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u drejones06 -p dckr_pat_wDWAxLoonygIJlzcpF75wsWL_d4
+    docker login -u drejones06 
     docker tag devops-build drejones06/dev
     docker push drejones06/dev
 
 elif [[ $GIT_BRANCH == "origin/master" ]]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u drejones06 -p dckr_pat_wDWAxLoonygIJlzcpF75wsWL_d4
+    docker login -u drejones06 
     docker tag devops-build drejones06/prod 
     docker push drejones06/prod
 else
-    echo "Failed"
+    echo "Execution failed ..! Exiting Script ..! Check deploy.sh file"
 fi
